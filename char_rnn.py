04689b20a27a55fdd789d5c158f1dfeb86b654e8
Jon Gauthier <jon@gauthiers.net>
2016-08-04 10:47:57 -0700

    init with functioning CharRNN

---
+++ char_rnn.py
@@ -0,0 +1,41 @@
+from rllab.algos.trpo import TRPO
+from rllab.algos.vpg import VPG
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.envs.normalized_env import normalize
+from rllab.misc.instrument import stub, run_experiment_lite
+from rllab.policies.categorical_gru_policy import CategoricalGRUPolicy
+
+from environments import WordEmissionEnvironment
+
+
+stub(globals())
+
+env = normalize(WordEmissionEnvironment("wordemit"), normalize_reward=True)
+
+policy = CategoricalGRUPolicy(
+        env_spec=env.spec,
+        hidden_sizes=(64,),
+        state_include_action=False,
+)
+
+baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+
+algo = TRPO(
+        env=env,
+        policy=policy,
+        baseline=baseline,
+        batch_size=4000,
+        max_path_length=5,
+        n_itr=50,
+        discount=0.99,
+        step_size=0.01,
+)
+
+
+run_experiment_lite(
+        algo.train(),
+        n_parallel=5,
+        snapshot_mode="last",
+        log_dir="./log",
+)
---
+++ environments.py
@@ -0,0 +1,79 @@
+import sys
+
+import numpy as np
+from rllab.envs.base import Env
+from rllab.envs.base import Step
+from rllab.spaces import Box, Discrete
+
+
+with open("wordsEn.txt", "r") as words_f:
+    WORDS = [line.strip() for line in words_f]
+
+
+class WordEmissionEnvironment(Env):
+
+    def __init__(self, *args, **kwargs):
+        super(WordEmissionEnvironment, self).__init__(*args, **kwargs)
+
+        self.vocab = WORDS
+        self._build_prefix_tree()
+
+        self.chars = list(set("".join(self.vocab)))
+        self.char2id = {c: idx for idx, c in enumerate(self.chars)}
+
+    def _build_prefix_tree(self):
+        prefix_tree = []
+        for item in self.vocab:
+            prefix_tree.extend(item[:prefix] for prefix in range(1, len(item) + 1))
+
+        self._prefix_tree = frozenset(prefix_tree)
+
+    @property
+    def observation_space(self):
+        return Box(low=0, high=1, shape=(len(self.chars),))
+
+    @property
+    def action_space(self):
+        return Discrete(len(self.chars))
+
+    def reset(self):
+        sys.stdout.flush()# DEV
+
+        # Select a random start character.
+        idx = np.random.randint(0, len(self.chars))
+        self._emitted = [self.chars[idx]]
+
+        # Observation: last output character
+        obs = np.zeros((len(self.chars),))
+        obs[idx] = 1
+        return obs
+
+    def step(self, action):
+        self._emitted.append(self.chars[action])
+
+        emitted_str = "".join(self._emitted)
+        valid_prefix = emitted_str in self._prefix_tree
+
+        done = False
+        if valid_prefix:
+            if len(self._emitted) > 2 and emitted_str in self.vocab:
+                done = True
+                reward = 2.0
+            else:
+                reward = 1.0
+        else:
+            done = True
+            reward = -1.0
+
+        if done and valid_prefix:
+            print emitted_str
+
+        # Sample another random observation.
+        # (Recurrent policy handles the state we care about..)
+        obs = np.zeros((len(self.chars),))
+        obs[action] = 1
+
+        return Step(observation=obs, reward=reward, done=done)
+
+    def render(self):
+        print "".join(self._emitted)
