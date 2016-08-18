6ea829bb60db23f72a7491214f19f5f2dc3d8521
Jon Gauthier <jon@gauthiers.net>
2016-08-18 10:31:20 -0700

    Stage missing files related to char autoencoder script (still broken)

---
+++ praglang/scripts/char_autoencoder.py
@@ -0,0 +1,56 @@
+"""
+Learn a recurrent policy which outputs valid English character sequences.
+The environment rewards the agent for outputting valid prefixes, and the
+agent quickly learns a simple character-level English language model.
+
+Unfortunately the entropy of the action space is rather low. After ~20
+iterations policy outputs ~170 unique words; only 30-40 of them are valid.
+"""
+
+
+from rllab.algos.trpo import TRPO
+from rllab.algos.vpg import VPG
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.envs.normalized_env import normalize
+from rllab.misc.instrument import stub, run_experiment_lite
+
+from praglang.environments import AutoencoderEnvironment
+from praglang.policies import EncoderDecoderPolicy
+
+
+stub(globals())
+
+LENGTH = 3
+VOCAB = list("abcde")
+
+autoenc_env = AutoencoderEnvironment(VOCAB, LENGTH, "autoenc")
+env = normalize(autoenc_env, normalize_reward=True)
+
+policy = EncoderDecoderPolicy(
+        env_spec=env.spec,
+        num_timesteps=LENGTH,
+        vocab_size=len(VOCAB),
+        hidden_sizes=(128,),
+)
+
+baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+
+algo = TRPO(
+        env=env,
+        policy=policy,
+        baseline=baseline,
+        batch_size=50,#000,
+        max_path_length=LENGTH,
+        n_itr=50,
+        discount=0.99,
+        step_size=0.01,
+)
+
+
+run_experiment_lite(
+        algo.train(),
+        n_parallel=1,#5,
+        snapshot_mode="last",
+        log_dir="./log/autoenc",
+)
---
+++ praglang/util.py
@@ -0,0 +1,67 @@
+from lasagne import init as LI
+from lasagne import layers as L
+from lasagne import nonlinearities as NL
+import theano
+
+from rllab.misc import ext
+
+
+class GRUStepLayer(L.MergeLayer):
+    """
+    A gated recurrent unit implements the following update mechanism:
+    Reset gate:        r(t) = f_r(x(t) @ W_xr + h(t-1) @ W_hr + b_r)
+    Update gate:       u(t) = f_u(x(t) @ W_xu + h(t-1) @ W_hu + b_u)
+    Cell gate:         c(t) = f_c(x(t) @ W_xc + r(t) * (h(t-1) @ W_hc) + b_c)
+    New hidden state:  h(t) = (1 - u(t)) * h(t-1) + u_t * c(t)
+    Note that the reset, update, and cell vectors must have the same dimension as the hidden state
+    """
+
+    def __init__(self, incomings,
+                 hidden_nonlinearity=NL.tanh,
+                 gate_nonlinearity=NL.sigmoid, name=None,
+                 W_init=LI.HeUniform(), b_init=LI.Constant(0.)):
+
+        if hidden_nonlinearity is None:
+            hidden_nonlinearity = LN.identity
+
+        if gate_nonlinearity is None:
+            gate_nonlinearity = LN.identity
+
+        if len(incomings) > 2:
+            # Multiple input values.
+            all_inputs, hid_prev = incomings[:-1], incomings[-1]
+            input_layer = L.ConcatLayer(all_inputs, name="concat_in_gru_step")
+            incomings = [input_layer, hid_prev]
+
+        super(GRUStepLayer, self).__init__(incomings, name=name)
+
+        input_dim = incomings[0].output_shape[-1]
+        num_units = incomings[1].output_shape[-1]
+
+        # Weights for the reset gate
+        self.W_xr = self.add_param(W_init, (input_dim, num_units), name="W_xr")
+        self.W_hr = self.add_param(W_init, (num_units, num_units), name="W_hr")
+        self.b_r = self.add_param(b_init, (num_units,), name="b_r", regularizable=False)
+        # Weights for the update gate
+        self.W_xu = self.add_param(W_init, (input_dim, num_units), name="W_xu")
+        self.W_hu = self.add_param(W_init, (num_units, num_units), name="W_hu")
+        self.b_u = self.add_param(b_init, (num_units,), name="b_u", regularizable=False)
+        # Weights for the cell gate
+        self.W_xc = self.add_param(W_init, (input_dim, num_units), name="W_xc")
+        self.W_hc = self.add_param(W_init, (num_units, num_units), name="W_hc")
+        self.b_c = self.add_param(b_init, (num_units,), name="b_c", regularizable=False)
+        self.gate_nonlinearity = gate_nonlinearity
+        self.num_units = num_units
+        self.nonlinearity = hidden_nonlinearity
+
+    def get_output_for(self, inputs, **kwargs):
+        x, hprev = inputs
+
+        r = self.gate_nonlinearity(x.dot(self.W_xr) + hprev.dot(self.W_hr) + self.b_r)
+        u = self.gate_nonlinearity(x.dot(self.W_xu) + hprev.dot(self.W_hu) + self.b_u)
+        c = self.nonlinearity(x.dot(self.W_xc) + r * (hprev.dot(self.W_hc)) + self.b_c)
+        h = (1 - u) * hprev + u * c
+        return h.astype(theano.config.floatX)
+
+    def get_output_shape_for(self, input_shape):
+        return input_shape[0], self.num_units
