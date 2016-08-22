2d34bd570f7ca72231d0555f3b548d051ee5ec60
Jon Gauthier <jon@gauthiers.net>
2016-08-22 12:12:10 -0700

    Update envs, adding BagAutoencoder environment

--- praglang/environments/autoencoder.py
+++ praglang/environments/autoencoder.py
@@ -3,10 +3,67 @@ import sys
 
 import numpy as np
 from rllab.envs.base import Env, Step
-from rllab.spaces.box import Box
-from rllab.spaces.discrete import Discrete
+from sandbox.rocky.tf.spaces.discrete import Discrete
 
 from praglang.spaces import DiscreteSequence
+
+
+
+class BagAutoencoderEnvironment(Env):
+
+    stop_on_error = False
+
+    # Only supports sparse rewards
+    sparse_rewards = True
+
+    def __init__(self, chars="abc", max_length=3, *args, **kwargs):
+        super(BagAutoencoderEnvironment, self).__init__(*args, **kwargs)
+
+        self.vocab = list(chars)
+        self.max_length = max_length
+
+        # Need to be able to form a sequence of max length without repeats
+        assert len(self.vocab) >= self.max_length
+
+    @property
+    def observation_space(self):
+        return DiscreteBinaryBag(len(self.vocab))
+
+    @property
+    def action_space(self):
+        return Discrete(len(self.vocab))
+
+    def reset(self):
+        # Sample length, then randomly pick chars.
+        length = np.random.randint(1, self.max_length + 1)
+
+        input_val = np.zeros((len(self.vocab),))
+        idxs = np.random.choice(len(self.vocab), replace=False, size=length)
+        input_val[idxs] = 1
+
+        self._input = input_val
+        self._reference = set([self.vocab[idx] for idx in idxs])
+        self._emitted = []
+        return self._input
+
+    def step(self, action):
+        self._emitted.append(action)
+
+        done = len(self._emitted) == len(self._input)
+        if self.stop_on_error:
+            done = done or action not in self._reference
+
+        reward = 0.0
+        if done:
+            reward = float(set(self._emitted) == self._reference)
+
+        return Step(observation=self._input, reward=reward, done=done)
+
+    def render(self):
+        output_chars = [self.vocab[idx] for idx in self._emitted]
+
+        print "% 3s\t=>\t% 3s" % ("".join(sorted(self._reference)),
+                                  "".join(sorted(output_chars)))
 
 
 class AutoencoderEnvironment(Env):
@@ -23,7 +80,6 @@ class AutoencoderEnvironment(Env):
     @property
     def observation_space(self):
         return DiscreteSequence(len(self.vocab), self.max_length)
-#        return Box(low=0, high=1, shape=(self.max_length, len(self.vocab)))
 
     @property
     def action_space(self):
